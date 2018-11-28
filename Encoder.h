4476aa56954e48a51a5f61fd7b6736e0b1e46cf6
Michael Essmyer <mde9rb@mail.umkc.edu>
2018-11-28 13:32:14 -0600

    Create Encoder.h

---
+++ Encoder.h
@@ -0,0 +1,17 @@
+//Encoder Interface
+
+//Encoder constructor (set pin input)
+
+//Check Encoder
+
+//Check Encoder w/values flipped
+
+//Get count (count encoder values)
+
+//Set count
+
+//Reset count
+
+//increment encoder
+
+//decrement encoder
