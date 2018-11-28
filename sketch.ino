6e70a6c2f9637ae687645b3e0393f67f5b1fb53c
Michael Essmyer <mde9rb@mail.umkc.edu>
2018-11-28 13:22:13 -0600

    Update and rename sketch to sketch.ino
    this is the main

---
+++ sketch.ino
@@ -0,0 +1,13 @@
+//include motor driver header
+//include encoder header
+
+//define motor driver pins
+//define IR pins
+//define LF pins
+
+//pass pin infomation to correct classes
+
+//setup
+//attach encoder pin interupts
+
+//loop
