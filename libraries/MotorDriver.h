4c6b50bf8dcc7a6d636cba5b14bc5f56c7a5af8b
Michael Essmyer <mde9rb@mail.umkc.edu>
2018-12-01 11:36:53 -0600

    Create MotorDriver.h

---
+++ libraries/MotorDriver.h
@@ -0,0 +1,15 @@
+//MotorDriver Constructor
+
+//set power
+
+//set direction
+
+//increment speed
+
+//decrement speed
+
+//calculate speed to give motor
+
+//check speed is occuring
+
+//On-off switch control aka reset
